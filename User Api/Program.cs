cee1f2cd6987e54a4ef4f0cc085f0465506a266e
Florian Fröhlich <florian.froehlich@hotmail.com>
2025-05-14 13:24:46 +0200

    Added Functionality

--- User Api/Program.cs
+++ User Api/Program.cs
@@ -2,5 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("downloads", () => "Downloads");
+app.MapPut("/", () => "This is a put");
+app.MapDelete("/", () => "This is a delete");
+app.MapGet("/users/{userId}/posts/{slug}", (int userId, string slug) =>{
+    return $"User ID: {userId}, Post ID: {slug}";
+    
+});
+app.MapPost("/", () => "This is a post");
 app.Run();
